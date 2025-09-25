81f70fc32ef96b818bdd1fee239032bff2ed80cd
SurendraReddy-KisanDesk <surendrareddy@kisandesk.com>
2025-09-25 19:58:19 +0530

    changed the port number

--- lib/views/services/api_config.dart
+++ lib/views/services/api_config.dart
@@ -1,9 +1,9 @@
 class KD {
-  static const String api = 'http://13.204.87.75:6000/api';
+  static const String api = 'http://13.204.87.75:7000/api';
 
   // Use the API like this
   //'${KD.api}/<rest content>' ---> e.g '${KD.api}/admin/fetch_mandi_rates'
-  //bascially KD.api  = http://13.233.103.50:6000/api
+  //bascially KD.api  = http://13.233.103.50:7000/api
 
   static Map<String, String> get headers => {
         'Content-Type': 'application/json',
